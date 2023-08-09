1ec007a6e9de7149064e050b3c638b8c2296f798
ejsadiarin <ejsadiarin@gmail.com>
2023-08-09 13:37:03 +0800

    Some service

--- backend/ExerciseTracker.API/API/Services/ExerciseService.cs
+++ backend/ExerciseTracker.API/API/Services/ExerciseService.cs
@@ -1,15 +1,44 @@
+using API.Data;
 using API.Models;
 
 namespace API.Services;
 
 public class ExerciseService : IExerciseService
 {
-    // 
+    // Constructor Injection
+    private readonly ExerciseDbContext _exerciseDbContext;
+
+    public ExerciseService(ExerciseDbContext exerciseDbContext) 
+    {
+        _exerciseDbContext = exerciseDbContext;
+    }
+
+
     public async Task<List<Exercise>> GetAllExercises()
     {
         // from Database, query all exercises data
-        var exercises = await   
+        var exercises = await _exerciseDbContext.Exercises.ToListAsync();   
+        return exercises;
     }
     
-    
+    public async Task<Exercise>? GetSingleExercise()
+    {
+        
+    }
+
+    public async Task<List<Exercise>>? AddExercise()
+    {
+
+    }
+
+    public async Task<List<Exercise>?> UpdateExercise();
+    {
+
+    }
+
+    public async Task<List<Exercise>?> DeleteExercise();
+    {
+
+    }
+
 }
