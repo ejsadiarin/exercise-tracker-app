67ef3848d7f0bf5e4c9ea8c4c1639d460694f2c9
ejsadiarin <ejsadiarin@gmail.com>
2023-08-11 11:52:11 +0800

    finished remaining controllers, TODO documentation

--- backend/ExerciseTracker.API/API/Controllers/ExerciseController.cs
+++ backend/ExerciseTracker.API/API/Controllers/ExerciseController.cs
@@ -23,13 +23,47 @@ public class ExerciseController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<Exercise>> GetExercise(int id)
     {
-        var result = await _exerciseService.GetSingleExercise(id);
+        // further refactor: type should be of DTO not the model Exercise itself --> to exclude id
+        Exercise? result = await _exerciseService.GetSingleExercise(id);
         
         if (result is null)
         {
             return NotFound("Specified exercise doesn't exist");
         }
 
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<List<Exercise>>> Add(Exercise exercise)
+    {
+        var result = await _exerciseService.AddExercise(exercise);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<List<Exercise>>> Update(int id, Exercise exercise)
+    {
+        var result = await _exerciseService.UpdateExercise(id, exercise);
+
+        if (result is null)
+        {
+            return NotFound($"Id {id} is not found.");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<List<Exercise>>> Delete(int id)
+    {
+        var result = await _exerciseService.DeleteExercise(id);
+
+        if (result is null)
+        {
+            return NotFound($"Id {id} is not found.");
+        }
 
         return Ok(result);
     }
