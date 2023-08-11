2f0164c1a29d8fc18bda0e30d6c5898ed9216ceb
ejsadiarin <ejsadiarin@gmail.com>
2023-08-11 11:36:37 +0800

    initialized dependency injection

--- backend/ExerciseTracker.API/API/Program.cs
+++ backend/ExerciseTracker.API/API/Program.cs
@@ -1,4 +1,9 @@
+using API.Data;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IExerciseService, ExerciseService>();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ExerciseDbContext>(opts =>
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("ExerciseTrackerDbConnection")));
 
 var app = builder.Build();
 
